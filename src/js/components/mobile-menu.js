f3df277cd9dee13a604109612483ebb9bd222c56
Anya-developer <anna.coder316@gmail.com>
2025-08-12 12:32:08 +0300

    replace slider images

--- src/js/components/mobile-menu.js
+++ src/js/components/mobile-menu.js
@@ -12,7 +12,7 @@ const mobileMenu =(buttonSelector, menuSelector,) =>{
 
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = "";
+        link.style.animation = ""; 
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${
           index / 7 + 0.5
